8bb8cf8eba5b02afafd1e1dacbe305984ac766b2
compfixengineer <compfixengineer@gmail.com>
2024-02-26 23:27:40 +0400

    add: swiper-slider with benefits block futeres

--- js/main.js
+++ js/main.js
@@ -3,6 +3,34 @@ const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    autoHeight: true,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".slider-button-next",
+        prevEl: ".slider-button-prev",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        576: {
+          slidesPerView: 2,
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 3,
+        },
+        // when window width is >= 1024px
+        1024: {
+          slidesPerView: 4,
+        },
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 5,
+          }
+    }   
+  });
+  
 
 const lightModeOn = (event) => {
     navbar.classList.add("navbar-light");
