83d39f1bf5a7781d5d1aa6baec44d29dfcd59641
compfixengineer <compfixengineer@gmail.com>
2024-03-10 20:16:35 +0400

    add: section Blog

--- js/main.js
+++ js/main.js
@@ -106,3 +106,36 @@ mMenuToggle.addEventListener("click", (event) => {
     //     openMenu();
     // }
 });
+
+
+const swiperBlog = new Swiper(".blog-slider",{
+  speed: 400,
+  spaceBetween: 30,
+  slidesPerView: 2,
+  navigation: {
+    nextEl: ".blog-button-next",
+    prevEl: ".blog-button-prev",
+},
+breakpoints: {
+  // when window width is >= 320px
+  320: {
+    slidesPerView: 1,
+  },
+  // when window width is >= 320px
+  576: {
+    slidesPerView: 1,
+  },
+  // when window width is >= 768px
+  768: {
+    slidesPerView: 1,
+  },
+  // when window width is >= 1024px
+  1024: {
+    slidesPerView: 1,
+  },
+  // when window width is >= 1200px
+  1200: {
+      slidesPerView: 2,
+    }
+}
+});
