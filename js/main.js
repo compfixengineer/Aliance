b76a4c0103329f31bdc79003ed7ba402b9156f8a
compfixengineer <compfixengineer@gmail.com>
2024-03-17 22:05:12 +0400

    add: modal window

--- js/main.js
+++ js/main.js
@@ -139,3 +139,18 @@ breakpoints: {
     }
 }
 });
+
+const modal = document.querySelector(".modal");
+const modalToggle = document.querySelectorAll("[data-toggle=modal]");
+const modalClose = document.querySelector(".modal-close");
+modalToggle.forEach((element) => {
+  element.addEventListener ("click",(event) => {
+    event.preventDefault();
+    modal.classList.add("is-open");
+  });
+});
+
+modalClose.addEventListener("click", (event) => {
+  event.preventDefault();
+  modal.classList.remove("is-open");
+} )
