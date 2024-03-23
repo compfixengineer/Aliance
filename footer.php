ed0bf7bda8abb3d1596d0f1b42c188236683377b
compfixengineer <compfixengineer@gmail.com>
2024-03-23 21:10:21 +0400

    add: handler telegram bot

--- footer.php
+++ footer.php
@@ -9,6 +9,8 @@
                        <div class="input-group">
                             <input id="user-name" type="text" class="input" 
                             name = "username"
+                            required
+                            maxlength="30"
                             placeholder=" ">
                             <label class="input-group-label" for="user-name">Имя</label>   
                        </div>
@@ -16,6 +18,8 @@
                        <div class="input-group">
                             <input id="user-phone" type="tel" class="input" 
                             name = "userphone"
+                            required
+                            maxlength="30"
                             placeholder=" ">
                             <label class="input-group-label" for="user-phone">Номер телефона</label>
                        </div>
@@ -173,16 +177,22 @@
             <p class="modal-text">Оставьте заявку, наш менеджер свяжется с Вами в ближайшее время ответит на все интересующие вопросы и поможем даже в самых сложных случаях!
             </p>
             <!-- /.modal-text -->
-            <form action="#" class="modal-form">
+            <form action="handler.php" method ="POST" class="modal-form">
                 <div class="input-group-wrapper input-group-vertical">
                     <div class="input-group modal-input-group">
                          <input id="modal-user-name" type="text" class="input modal-input"
+                         name = "username"
+                         required
+                         maxlength="30"
                          placeholder=" ">
                          <label class="input-group-label modal-input-lable" for="modal-user-name">Имя</label>   
                     </div>
                     <!-- /.input-group -->
                     <div class="input-group modal-input-group">
                          <input id="modal-user-phone" type="tel" class="input modal-input"
+                         name = "userphone"
+                         required
+                         maxlength="30"
                          placeholder=" ">
                          <label class="input-group-label modal-input-lable" for="modal-user-phone">Номер телефона</label>
                     </div>
--- handler.php
+++ handler.php
@@ -1,5 +1,23 @@
 <?php 
-$user_name = $_POST["username"];
-$user_phone = $_POST["userphone"];
-echo "Привет, " . $user_name . "<br>";
-echo "Ваш телефон: ".$user_phone;
+$user_name = htmlspecialchars($_POST["username"]);
+$user_phone = htmlspecialchars($_POST["userphone"]);
+
+$token = "7144234716:AAGZsZOg1mQCFVAP8Q1wVvjk4vaa7JN8O3g";
+$chat_id = "-4180955396";
+
+$form_data = array(
+    "Клиент: " => $user_name,
+    "Телефон: " => $user_phone
+);
+
+foreach ($form_data as $key => $value) {
+    $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A";
+}
+
+$send_to_telegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=HTML","r");
+
+if ($send_to_telegram) {
+    echo "Success";
+} else {
+    echo "Error";
+}
